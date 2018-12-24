83edfbcbdde556eadd62b31c6044c4f506482cf9
n.o.bialik <n.o.bialik@gmail.com>
2018-12-24 22:58:31 +0200

    create person + add in Trainee

---
+++ BE/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Person
+{
+
+    private string id;
+    private string first_name;
+    private string last_name;
+    private DateTime birthdate;
+    private Gender gender;
+    private string phone_number;
+    private Address address;
+
+    public string Id { get => id; set => id = value; }
+    public string First_name { get => first_name; set => first_name = value; }
+    public string Last_name { get => last_name; set => last_name = value; }
+    public DateTime Birthdate { get => birthdate; set => birthdate = value; }
+    public Gender Gender { get => gender; set => gender = value; }
+    public string Phone_number { get => phone_number; set => phone_number = value; }
+    public Address Address { get => address; set => address = value; }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
--- BE/trainee.cs
+++ BE/trainee.cs
@@ -8,6 +8,19 @@ namespace BE
 {
     public class Trainee
     {
+        private string id;
+        private string first_name;
+        private string last_name;
+        private DateTime birthdate;
+        private Gender gender;
+        private string phone_number;
+        private Address address;
 
+        //we need to add more properies
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
