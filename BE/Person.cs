16c5ef1efda6709f2ee20a580af422c9a3ab5cd4
אביעד דוידי <aviad.davidi1@gmail.com>
2018-12-25 19:57:20 +0200

    i done person and tester

--- BE/Person.cs
+++ BE/Person.cs
@@ -16,14 +16,21 @@ namespace BE
         public string Id { get => id; set => id = value; }
         public string First_name { get => first_name; set => first_name = value; }
         public string Last_name { get => last_name; set => last_name = value; }
-        public DateTime Birthdate { get => birthdate; set => birthdate = value; }
+        public DateTime Birthdate
+        {
+            get => birthdate;
+            set
+            {
+                birthdate = new DateTime(value.Year, value.Month, value.Day);
+            }
+        }
         public Gender Gender { get => gender; set => gender = value; }
         public string Phone_number { get => phone_number; set => phone_number = value; }
         public Address Address { get => address; set => address = value; }
 
-        public override string ToString()
+        public virtual string ToString()
         {
-            return base.ToString();
+            return "id: " + Id + "first name: " + First_name + "last name: " + Last_name;
         }
     }
 }
--- BE/Tester.cs
+++ BE/Tester.cs
@@ -4,65 +4,14 @@ using System.Text;
 
 namespace BE
 {
-    class Tester
+    class Tester : Person
     {
-
-        private string id;
-        private string first_name;
-        private string last_name;
-        private DateTime birthdate;
-        private Gender gender;
-        private string phone_number;
-        private Address address;
         private double experience;//how many years of experience the tester has 
         private int max_tests_per_week;
         private Vehicle speciality;
         private bool[][] schedule;
         private double max_range;
         
-        public string Id { get => id; set => id = value; }
-        public string First_name { get => first_name; set => first_name = value; }
-        public string Last_name { get => last_name; set => last_name = value; }
-        public DateTime Birthdate
-        {
-            get => birthdate;
-            set
-            {
-                //check if the tester is in the right age
-                //....
-                birthdate = new DateTime(value.Year, value.Month, value.Day);
-            }
-        }
-        public string Phone_number
-        {
-            get
-            {
-                //return the number in the format 05x-xxx-xxxx
-                string ret = phone_number.Substring(0, 3) + '-';
-                ret += phone_number.Substring(3, 3) + '-';
-                ret += phone_number.Substring(6);
-                return ret;
-            }
-            set
-            {
-                //check if the phone number contain only digits
-                try
-                {
-                    int.Parse(value);
-                }
-                catch (Exception)
-                {
-
-                    throw new Exception("the phone number must contain only digits");
-                }
-                //check if the number in the format 05x-xxx-xxxx
-                if (!(value.Length == 10 && value.Substring(0, 2) == "05"))
-                    throw new Exception("the phone number must be in the format 05x-xxx-xxxx");
-                //if the number correct save him
-                phone_number = value;
-            }
-        }
-        public Address Address { get => address; set => address = value; }
         public double Experience { get => experience; set => experience = value; }
         public int Max_tests_per_week { get => max_tests_per_week; set => max_tests_per_week = value; }
         public bool[][] Schedules
@@ -98,7 +47,7 @@ namespace BE
         public Vehicle Speciality { get => speciality; set => speciality = value; }
 
 
-        public Tester(string id, string first_name, string last_name, DateTime birthdate, string phone_number, Address address, double experience, int max_tests_per_week, double max_range, Gender gender, Vehicle speciality, bool[][] schedule = null)
+        public Tester(double experience, int max_tests_per_week, double max_range, Gender gender, Vehicle speciality, bool[][] schedule = null)
         {
             //create schedule array
             this.schedule = new bool[5][];
@@ -111,11 +60,6 @@ namespace BE
             //try to initialize the variables
             try
             {
-                Id = id;
-                First_name = first_name;
-                Last_name = last_name;
-                Birthdate = birthdate;
-                Address = address;
                 Experience = experience;
                 Max_tests_per_week = max_tests_per_week;
                 Max_range = max_range;
@@ -127,13 +71,12 @@ namespace BE
             }
             catch (Exception)
             {
-
                 throw;
             }
         }
         public override string ToString()
         {
-            return "id: "+Id + "\n"+"name: " + Last_name + " " + First_name;
+            return base.ToString();
         }
     }
 }
